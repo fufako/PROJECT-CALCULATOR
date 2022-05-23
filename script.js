e9880fe5b2fe409891f95674a80ebc0fc1b13c5e
fufako <patryko.mikiciuk@gmail.com>
2022-05-23 19:48:59 +0200

    Add del, clear functions update operatorPressed and operate functions

--- script.js
+++ script.js
@@ -2,51 +2,73 @@ let currNumb = ""
 let prevNumb = ""
 let operator = ""
 
-const numbers = document.querySelectorAll(".number")
-for (number of numbers) {
+const operators = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  x: (a, b) => a * b,
+  "÷": (a, b) => a / b,
+  "": () => 0,
+}
+
+document.querySelectorAll(".number").forEach((number) => {
   number.onclick = (e) => store(e.target.textContent)
-}
-const operations = document.querySelectorAll(".operation")
-for (operation of operations) {
-  operation.onclick = (e) => operatorPressed(e.target.id)
-}
+})
+
+document.querySelectorAll(".operation").forEach((operation) => {
+  operation.onclick = (e) => operatorPressed(e.target.textContent)
+})
 
 const dotBtn = document.querySelector(".dot")
 const equalsBtn = document.querySelector(".equals")
+const currOperation = document.querySelector(".currentOperation")
+const lastOperation = document.querySelector(".lastOperation")
+const clearBtn = document.querySelector(".clear")
+const deleteBtn = document.querySelector(".delete")
+
+clearBtn.onclick = () => clear()
+deleteBtn.onclick = () => del()
 
 equalsBtn.onclick = () => operate()
 
 function store(number) {
   currNumb += number
+  currOperation.textContent = currNumb
 }
 
-function operatorPressed(action) {
+function operatorPressed(currOperator) {
+  lastOperation.textContent = currNumb + " " + currOperator + " "
   prevNumb = currNumb
   currNumb = ""
-  operator = action
+  operator = currOperator
 }
 
 function operate() {
+  // Don't do this V
+  // const [a, b] = [prevNumb, currNumb].map(Number)
+  const a = Number(prevNumb)
+  const b = Number(currNumb)
+  if (operator === "") {
+    currOperation.textContent = 0
+    lastOperation.textContent = ""
+  }
+  const result = operators[operator](a, b)
+  lastOperation.textContent += currNumb
+  currOperation.textContent = result
+  prevNumb = ""
+  currNumb = String(result)
+  operator = ""
+}
+
+function clear() {
   prevNumb = ""
   currNumb = ""
   operator = ""
+  lastOperation.textContent = ""
+  currOperation.textContent = "0"
 }
-function add(a, b) {
-  return a + b
+function del() {
+  currNumb = currNumb.slice(0, -1)
+  currOperation.textContent = currNumb
 }
-
-function subtract(a, b) {
-  return a - b
-}
-
-function multiply(a, b) {
-  return a * b
-}
-
-function divide(a, b) {
-  return a / b
-}
-
-function clear() {}
 
 function main() {}
