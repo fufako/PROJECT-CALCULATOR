d95e446281e278491865026b7b4da4ec350068a9
fufako <patryko.mikiciuk@gmail.com>
2022-05-23 18:03:57 +0200

    Add loops for onclick events, add math functions

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
 let currNumb = ""
 let prevNumb = ""
+let operator = ""
 
-const oneBtn = document.querySelector(".one")
-const twoBtn = document.querySelector(".two")
-const threeBtn = document.querySelector(".three")
-const fourBtn = document.querySelector(".four")
-const fiveBtn = document.querySelector(".five")
-const sixBtn = document.querySelector(".six")
-const sevenBtn = document.querySelector(".seven")
-const eightBtn = document.querySelector(".eight")
-const nineBtn = document.querySelector(".nine")
-const zeroBtn = document.querySelector(".zero")
+const numbers = document.querySelectorAll(".number")
+for (number of numbers) {
+  number.onclick = (e) => store(e.target.textContent)
+}
+const operations = document.querySelectorAll(".operation")
+for (operation of operations) {
+  operation.onclick = (e) => operatorPressed(e.target.id)
+}
 
-const multiplyBtn = document.querySelector(".multiply")
-const divideBtn = document.querySelector(".divide")
-const minusBtn = document.querySelector(".minus")
-const plusBtn = document.querySelector(".plus")
 const dotBtn = document.querySelector(".dot")
 const equalsBtn = document.querySelector(".equals")
 
-oneBtn.onclick = (e) => store(e.target.id)
-twoBtn.onclick = (e) => store(e.target.id)
-threeBtn.onclick = (e) => store(e.target.id)
-fourBtn.onclick = (e) => store(e.target.id)
-fiveBtn.onclick = (e) => store(e.target.id)
-sixBtn.onclick = (e) => store(e.target.id)
-sevenBtn.onclick = (e) => store(e.target.id)
-eightBtn.onclick = (e) => store(e.target.id)
-nineBtn.onclick = (e) => store(e.target.id)
-zeroBtn.onclick = (e) => store(e.target.id)
-
-plusBtn.onclick = (e) => operatorPressed()
-minusBtn
 equalsBtn.onclick = () => operate()
+
 function store(number) {
   currNumb += number
 }
 
-function operatorPressed() {
+function operatorPressed(action) {
   prevNumb = currNumb
   currNumb = ""
+  operator = action
 }
+
 function operate() {
-  console.log(parseInt(prevNumb) + parseInt(currNumb))
   prevNumb = ""
   currNumb = ""
+  operator = ""
+}
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
 }
 
 function clear() {}
